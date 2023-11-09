a66e333b4fbcc2d72220b0a64d271e85e78c92aa
Deestr0yeer <150370440+Deestr0yeer@users.noreply.github.com>
2023-11-09 17:49:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,34 +6,45 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var slides = [
   {
-    location: [48.8566, 2.3522], // Paryż
-    description: "Eiffla Tower i wiele więcej..."
+    location: [44.427963, -110.588455], // Yellowstone National Park, USA
+    description: "Odkryj cuda Parku Narodowego Yellowstone w USA."
   },
   {
-    location: [-16.5000, -151.7500], // Bora Bora
-    description: "Egzotyczne plaże i relaks..."
+    location: [51.4968, -115.9281], // Banff National Park, Kanada
+    description: "Zwiedź zapierające dech w piersiach krajobrazy Parku Narodowego Banff w Kanadzie."
   },
   {
-    location: [27.9878, 86.9250], // Himalaje
-    description: "Wspinaczka w Himalajach - majestatyczne szczyty..."
+    location: [44.8659, 15.5820], // Plitvice Lakes National Park, Chorwacja
+    description: "Podziwiaj kaskadowe wodospady Parku Narodowego Jezior Plitwickich w Chorwacji."
   },
   {
-    location: [-2.1540, 34.6857], // Serengeti
-    description: "Safari w Serengeti - dzikie zwierzęta na wolności..."
+    location: [-0.9537, -91.0966], // Galápagos National Park, Ekwador
+    description: "Spotkaj unikalną faunę w Parku Narodowym Galapagos w Ekwadorze."
   },
   {
-    location: [-13.1631, -72.5450], // Machu Picchu
-    description: "Podróż do Machu Picchu - historyczne miejsce na szczycie gór..."
+    location: [-51.2592, -73.2323], // Torres del Paine National Park, Chile
+    description: "Doświadcz oszałamiającej urody Parku Narodowego Torres del Paine w Chile."
   },
   {
-    location: [-16.5000, 145.7400], // Wielka Rafa Koralowa
-    description: "Podwodna przygoda w Wielkiej Rafie Koralowej - kolorowe rafy i bogactwo morskiego życia..."
+    location: [-2.1540, 34.6857], // Serengeti National Park, Tanzania
+    description: "Obserwuj widowisko przyrody w Parku Narodowym Serengeti w Tanzanii."
   },
   {
-    location: [35.6895, 139.6917], // Japonia
-    description: "Wycieczka po Japonii - kulturowe atrakcje i piękne krajobrazy..."
+    location: [-18.2866, 147.6992], // Great Barrier Reef Marine Park, Australia
+    description: "Zanurz się w barwnym życiu morskim Wielkiej Rafy Koralowej w Australii."
   },
-  // Dodaj kolejne lokalizacje
+  {
+    location: [33.2588, 103.9180], // Jiuzhaigou Valley National Park, Chiny
+    description: "Odkryj kolorowe krajobrazy Parku Narodowego Doliny Jiuzhaigou w Chinach."
+  },
+  {
+    location: [37.2982, -113.0263], // Zion National Park, USA
+    description: "Wędruj przez majestatyczne kaniony Parku Narodowego Zion w USA."
+  },
+  {
+    location: [-23.9884, 31.5547], // Kruger National Park, RPA
+    description: "Poczuj ekscytację safari w Parku Narodowym Kruger w RPA."
+  }
 ];
 
 L.control.storymap({
